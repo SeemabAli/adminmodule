1db41c7a070bb1d9529a7baa63d1dc8cea70649d
muhammadali <muhammadalihassanofficial@gmail.com>
2025-04-07 11:13:23 +0500

    fix: add first trip zod to mandatory

--- src/pages/Accounts/TruckOtherExpenses/truckotherexpenses.schema.ts
+++ src/pages/Accounts/TruckOtherExpenses/truckotherexpenses.schema.ts
@@ -9,11 +9,13 @@ export const truckOtherExpensesSchema = z.object({
     .min(3, "Expense name must be at least 3 characters")
     .max(50, "Expense name must not exceed 50 characters"),
 
-  firstTrip: z.number().optional(),
+  firstTrip: z
+    .number()
+    .min(1, "First trip amount is required and must be greater than 0"),
 
-  secondTrip: z.number().optional(),
+  secondTrip: z.number().optional().nullable(),
 
-  thirdTrip: z.number().optional(),
+  thirdTrip: z.number().optional().nullable(),
 });
 
 // Type definitions
